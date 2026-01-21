50d2054736c483af4bad4b74119af7f20edd8a22
harshita-aggarwal <haggarw5@asu.edu>
2026-01-21 15:42:36 -0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api/notes", (req, res) => {
+    res.status(200).send("you got 5 notes");
+})
+
+app.listen(5001, () => {
+    console.log("Server started on Port: 5001");
+})
