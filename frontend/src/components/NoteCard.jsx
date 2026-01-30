a525964fed081bc3c8b1eed6db7a52a12692dcb3
harshita-aggarwal <haggarw5@asu.edu>
2026-01-29 16:10:25 -0800

    Create Page Functionality and UI

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,33 +1,49 @@
 import { Link } from "react-router";
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { formatDate } from "../lib/utils";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
 
-const NoteCard = ({note}) => {
-  return <Link to={`/note/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#9db787]">
+const NoteCard = ({ note, setNotes }) => {
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
 
-    <div className="card-body">
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
 
-        <h2 className="card-title text-info">
-            {note.title}
-        </h2>
-        <p className="text-base-content/70 line-clamp-3 ">
-            {note.content}
-        </p>
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter(note => note._id !== id))
+      toast.success("Note Deleted Successfully!!");
+    } catch (error) {
+        console.log("Error in handleDelete ", error);
+        toast.error("Failed to delete note!!");
+    }
+  };
+  return (
+    <Link
+      to={`/note/${note._id}`}
+      className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#9db787]"
+    >
+      <div className="card-body">
+        <h2 className="card-title text-info">{note.title}</h2>
+        <p className="text-base-content/70 line-clamp-3 ">{note.content}</p>
         <div className="card-actions justify-between items-center mt-4">
-            <span className="text-sm text-base-content/60">
-                {formatDate(new Date(note.createdAt))}
-            </span>
-            <div className="flex items-center gap-1">
-                <PenSquareIcon className="size-4 text-info" />
-                <button className="btn btn-ghost btn-xs text-error">
-                    <Trash2Icon className="size-4" />
-                </button>
-            </div>
-
+          <span className="text-sm text-base-content/60">
+            {formatDate(new Date(note.createdAt))}
+          </span>
+          <div className="flex items-center gap-1">
+            <PenSquareIcon className="size-4 text-info" />
+            <button className="btn btn-ghost btn-xs text-error">
+              <Trash2Icon
+                className="size-4"
+                onClick={(e) => handleDelete(e, note._id)}
+              />
+            </button>
+          </div>
         </div>
-
-    </div>
-  </Link>;
+      </div>
+    </Link>
+  );
 };
 
 export default NoteCard;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import toast from "react-hot-toast";
+import api from "../lib/axios";
 
 import RateLimitedUI from "../components/RateLimitedUI";
 import Navbar from "../components/Navbar";
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -14,7 +15,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
 
         console.log(res.data);
 
@@ -22,7 +23,7 @@ const HomePage = () => {
         setIsRateLimited(false);
       } catch (error) {
         console.error("Error Fetching notes!!");
-        if (error.response?.status === 429) {
+        if (error.response.status === 429) {
           setIsRateLimited(true);
         } else {
           toast.error("Failed to load notes");
@@ -41,12 +42,15 @@ const HomePage = () => {
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && (
-          <div className="text-center text-secondary py-10">Loading notes...</div>
+          <div className="text-center text-primary py-10">Loading notes...</div>
         )}
+        {
+          notes.length ===0 && !isRateLimited && <NotesNotFound />
+        }
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
         )}
